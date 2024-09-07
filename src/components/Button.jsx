605e9a603a5607ce578ec9f69e33ff3e3b623b0c
Gaurav Singh <139119394+GauRav-sINGh123@users.noreply.github.com>
2024-09-08 00:34:51 +0530

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Button() {
   return (
     <a
-      href="https://github.com/GauRav-sINGh123"
+      href="https://github.com/GauRav-sINGh123/MovieHub"
       target="_blank"
       className="flex overflow-hidden items-center text-sm font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-black text-white shadow border bottom-0 border-gray-700 hover:bg-black/90 h-9 px-4 py-2 max-w-52 whitespace-pre md:flex group relative w-full justify-center gap-2 rounded-md transition-all duration-300 ease-out hover:ring-2 hover:ring-black hover:ring-offset-2"
     >
